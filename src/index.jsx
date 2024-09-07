04d47a12e2f9f0a7ddbad97b173763484390a212
Collin Murch <collin@murch.net>
2024-09-07 18:33:10 -0500

    hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,19 @@
 /* @refresh reload */
 import { render, isServer } from 'solid-js/web';
-import { Router, Route } from "@solidjs/router";
-
+import { HashRouter, Route } from "@solidjs/router";
 import { Home, About, Work, Contact } from './pages';
-
 import './index.css';
-
 
 const root = document.getElementById("root");
 
 render(
   () => (
-    <Router url={isServer ? req.url : ""}>
+    <HashRouter url={isServer ? req.url : ""}>
       <Route path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/work" component={Work} />
       <Route path="/contact" component={Contact} />
-    </Router>
+    </HashRouter>
   ),
   root
 );
