512702bd377d4f623651d97b9e5b1e918c38548c
Collin Murch <collin@murch.net>
2025-11-27 17:09:10 +0000

    adjust height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -43,7 +43,7 @@
 			<Navigation />
 		</header>
 
-		<main class="flex-1 min-h-0 scrollbar-reserve pt-4 md:pt-6">
+		<main class="flex-1 min-h-0 scrollbar-reserve pt-7 md:pt-11">
 			{#key $page.url.pathname}
 				<section
 					class="page-shell max-lg:landscape:pb-12 max-lg:landscape:pt-16 md:pb-16"
